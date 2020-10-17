832a33cd0fe73d6921566d838b7e673fccca6ddd
Shashi Kumar D M <dnksshashi@gmail.com>
2020-10-17 13:05:08 -0400

    added the search btn in sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,11 @@ const navBtn = document.querySelector(".nav-btn");
 const hamBurger = document.querySelector(".hamburger");
 const darkBg = document.querySelector(".dark-back");
 const navBar = document.querySelector("nav");
+const searchBox = document.querySelector(".search-block input");
 
 navBtn.addEventListener("click",function(){
     hamBurger.classList.toggle("active");
     darkBg.classList.toggle("active");
     navBar.classList.toggle("active");
+    searchBox.classList.toggle("active");
 });
